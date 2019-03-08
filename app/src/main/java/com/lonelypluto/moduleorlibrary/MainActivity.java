b4f913b071165ca8d386c0f7f102bdacad785ae0
LonelyPluto <277857006@qq.com>
2019-03-08 16:21:36 +0800

    第一次提交

---
+++ app/src/main/java/com/lonelypluto/moduleorlibrary/MainActivity.java
@@ -0,0 +1,18 @@
+package com.lonelypluto.moduleorlibrary;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.lonelypluto.mylibrary.TestActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
